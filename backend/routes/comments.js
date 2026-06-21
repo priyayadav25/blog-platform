67e6bd502be7d6d7374753aa45ca649d9b2dd0da
priyayadav25 <priyay2519@gmail.com>
2026-06-21 17:59:51 +0530

    Update comments.js

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -1,1 +1,20 @@
+const router=require("express").Router();
+const Comment=require("../models/Comment");
 
+router.get("/:postId",async(req,res)=>{
+ const comments=await Comment.find({
+  postId:req.params.postId
+ });
+
+ res.json(comments);
+});
+
+router.post("/",async(req,res)=>{
+ const comment=new Comment(req.body);
+
+ await comment.save();
+
+ res.json(comment);
+});
+
+module.exports=router;
