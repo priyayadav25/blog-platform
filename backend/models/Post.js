3974d9468e3e7c9a322834faf7bfd5f2e4eaa8ff
priyayadav25 <priyay2519@gmail.com>
2026-06-21 17:56:00 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const PostSchema = new mongoose.Schema({
+ title:String,
+ content:String,
+ author:String
+});
+
+module.exports = mongoose.model("Post",PostSchema);
