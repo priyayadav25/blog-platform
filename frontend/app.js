14620504d48d4d2a6567c1a17aa882df877fe1ce
priyayadav25 <priyay2519@gmail.com>
2026-06-21 18:01:09 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,18 @@
+fetch("http://localhost:5000/api/posts")
+.then(res=>res.json())
+.then(data=>{
 
+ let output="";
+
+ data.forEach(post=>{
+  output+=`
+   <div class="post">
+   <h2>${post.title}</h2>
+   <p>${post.content}</p>
+   </div>
+  `;
+ });
+
+ document.getElementById("posts").innerHTML=
+ output;
+});
