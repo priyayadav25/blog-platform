6ad39768f86a6d7a2550e18c157e1e031ff6aa61
priyayadav25 <priyay2519@gmail.com>
2026-06-21 17:57:27 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,15 @@
+const jwt=require("jsonwebtoken");
 
+module.exports=(req,res,next)=>{
+ const token=req.header("token");
+
+ if(!token)
+ return res.status(401).json("Access Denied");
+
+ try{
+  jwt.verify(token,"secretkey");
+  next();
+ }catch{
+  res.status(400).json("Invalid Token");
+ }
+};
