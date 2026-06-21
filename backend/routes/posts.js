707d2b0ac561b095fce987e2046620bf72918ea9
priyayadav25 <priyay2519@gmail.com>
2026-06-21 17:57:49 +0530

    Update posts.js

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,1 +1,30 @@
+const router=require("express").Router();
+const Post=require("../models/Post");
+const auth=require("../middleware/auth");
 
+router.get("/",async(req,res)=>{
+ const posts=await Post.find();
+ res.json(posts);
+});
+
+router.post("/",auth,async(req,res)=>{
+ const post=new Post(req.body);
+ await post.save();
+ res.json(post);
+});
+
+router.put("/:id",auth,async(req,res)=>{
+ const post=await Post.findByIdAndUpdate(
+  req.params.id,
+  req.body,
+  {new:true}
+ );
+ res.json(post);
+});
+
+router.delete("/:id",auth,async(req,res)=>{
+ await Post.findByIdAndDelete(req.params.id);
+ res.json("Deleted");
+});
+
+module.exports=router;
