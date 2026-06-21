be560ab5aa38099988c318c746d77c372e4ec361
priyayadav25 <priyay2519@gmail.com>
2026-06-21 17:54:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+"mongodb://127.0.0.1:27017/blogplatform"
+);
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/comments", require("./routes/comments"));
+
+app.listen(5000, () => {
+ console.log("Server Running");
+});
