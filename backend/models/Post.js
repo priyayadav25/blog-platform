72cdd1d486ca8e4ad872ff339075a30caa52d765
priyayadav25 <priyay2519@gmail.com>
2026-06-26 20:58:37 +0530

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
 
-const PostSchema = new mongoose.Schema({
- title:String,
- content:String,
- author:String
+const PostSchema = new mongoose.Schema(
+{
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+
+    content:{
+        type:String,
+        required:true
+    },
+
+    author:{
+        type:String,
+        required:true
+    },
+
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    }
+},
+{
+    timestamps:true
 });
 
-module.exports = mongoose.model("Post",PostSchema);
+module.exports = mongoose.model("Post", PostSchema);
