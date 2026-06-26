437082043ddbf3661015ddfe047182a9cbc05190
priyayadav25 <priyay2519@gmail.com>
2026-06-26 20:59:23 +0530

    Update Comment.js

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,9 +1,32 @@
 const mongoose = require("mongoose");
 
-const CommentSchema = new mongoose.Schema({
- postId:String,
- username:String,
- comment:String
+const CommentSchema = new mongoose.Schema(
+{
+    postId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Post",
+        required:true
+    },
+
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+
+    username:{
+        type:String,
+        required:true
+    },
+
+    comment:{
+        type:String,
+        required:true,
+        trim:true
+    }
+},
+{
+    timestamps:true
 });
 
-module.exports = mongoose.model("Comment",CommentSchema);
+module.exports = mongoose.model("Comment", CommentSchema);
